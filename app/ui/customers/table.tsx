dcc0b6c9c4a22382559352e2db40cc7cc8fd017e
Álvaro Ortí Segovia <alvaroorti7@gmail.com>
2023-11-21 16:03:17 +0100

    vercel

--- app/ui/customers/table.tsx
+++ app/ui/customers/table.tsx
@@ -31,7 +31,7 @@ export default async function CustomersTable({
                             <Image
                               src={customer.image_url}
                               className="rounded-full"
-                              alt={`profile picture`}
+                              alt={`${customer.name}'s profile picture`}
                               width={28}
                               height={28}
                             />
@@ -85,13 +85,13 @@ export default async function CustomersTable({
                     <tr key={customer.id} className="group">
                       <td className="whitespace-nowrap bg-white py-5 pl-4 pr-3 text-sm text-black group-first-of-type:rounded-md group-last-of-type:rounded-md sm:pl-6">
                         <div className="flex items-center gap-3">
-                        <Image
-                              src={customer.image_url}
-                              className="rounded-full"
-                              alt={`profile picture`}
-                              width={28}
-                              height={28}
-                            />
+                          <Image
+                            src={customer.image_url}
+                            className="rounded-full"
+                            alt={`${customer.name}'s profile picture`}
+                            width={28}
+                            height={28}
+                          />
                           <p>{customer.name}</p>
                         </div>
                       </td>
