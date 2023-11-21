8adad16c027dfb3610311109846a40017f10d890
Álvaro Ortí Segovia <alvaroorti7@gmail.com>
2023-11-21 17:41:11 +0100

    vercel

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -26,6 +26,7 @@ export default async function InvoicesTable({
                 <div className="flex items-center justify-between border-b pb-4">
                   <div>
                     <div className="mb-2 flex items-center">
+                    /* eslint-disable jsx-a11y/alt-text */
                       <Image
                         src={invoice.image_url}
                         className="mr-2 rounded-full"
@@ -33,6 +34,7 @@ export default async function InvoicesTable({
                         height={28}
                         alt={`${invoice.name}'s profile picture`}
                       />
+                      /* eslint-enable jsx-a11y/alt-text */
                       <p>{invoice.name}</p>
                     </div>
                     <p className="text-sm text-gray-500">{invoice.email}</p>
@@ -85,12 +87,15 @@ export default async function InvoicesTable({
                 >
                   <td className="whitespace-nowrap py-3 pl-6 pr-3">
                     <div className="flex items-center gap-3">
+                    /* eslint-disable jsx-a11y/alt-text */
                       <Image
                         src={invoice.image_url}
                         className="rounded-full"
                         width={28}
                         height={28}
+                        alt={`${invoice.name}'s profile picture`}
                       />
+                      /* eslint-enable jsx-a11y/alt-text */
                       <p>{invoice.name}</p>
                     </div>
                   </td>
